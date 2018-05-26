8e5b3eba2d0e27413da4e26caaa2471c3479e146
jijokrishnan33 <jijokrishnan33@gmail.com>
2018-05-26 15:12:06 +0530

    Modification as per review

--- WorkOutTrackerService/src/main/java/com/cts/fsd/WorkoutTrackerServiceApplication.java
+++ WorkOutTrackerService/src/main/java/com/cts/fsd/WorkoutTrackerServiceApplication.java
@@ -2,10 +2,17 @@ package com.cts.fsd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WorkoutTrackerServiceApplication {
-	
+public class WorkoutTrackerServiceApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WorkoutTrackerServiceApplication.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(WorkoutTrackerServiceApplication.class, args);
 	}
